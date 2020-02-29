10a7f0cfb8af3c25b1c7c89c530ecb8c78666af7
Majid Ataee <ataiemajid_63@hotmail.com>
2020-02-29 21:18:16 +0330

    Create new test case.

--- tests/PasoonateTest.php
+++ tests/PasoonateTest.php
@@ -1,8 +1,14 @@
 <?php
 
+use Pasoonate\Pasoonate;
 use PHPUnit\Framework\TestCase;
 
 class CalendarTest extends TestCase
 {
-    
+    public function testJalaliDateTime()
+    {
+        $pasoonate = Pasoonate::make();
+
+        $this->assertEquals($pasoonate->jalali('1398/12/10')->format('yyyy/MM/dd'), '1398/12/10', 'Jalali Date is ok');
+    }
 }
