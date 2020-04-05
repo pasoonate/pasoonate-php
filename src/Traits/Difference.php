4f7b89a7528a735ffdd0881a2bd7c6ab819fa5fb
Majid Ataee <ataiemajid_63@hotmail.com>
2020-04-05 09:00:31 +0430

    Change calc difference algorithm.

--- src/Traits/Difference.php
+++ src/Traits/Difference.php
@@ -2,78 +2,101 @@
 
 namespace Pasoonate\Traits;
 
+use Pasoonate\Calendars\CalendarManager;
+use Pasoonate\Constants;
+
 trait Difference
 {
-    public function diff($secondInstance)
+    public function diff(CalendarManager $instance)
     {
-        $diffInSeconds = abs($this->getTimestamp() - $secondInstance->getTimestamp());
+        $diffInSeconds = $this->diffInSeconds($instance);
+        $diffInDays = $diffInSeconds / Constants::DAY_IN_SECONDS;
 
-        $years = floor($diffInSeconds / 31536000); // Constants->DaysOfJalaliYear * 24 * 60 * 60
-        $diffInSeconds %= 31536000;
-        $months = floor($diffInSeconds / 2592000); // 30 * 24 * 60 * 60
-        $diffInSeconds %= 2592000;
-        $days = floor($diffInSeconds / 86400); // 24 * 60 * 60
-        $diffInSeconds %= 86400;
-        $hours = floor($diffInSeconds / 3600); // 60 * 60
-        $diffInSeconds %= 3600;
-        $minutes = floor($diffInSeconds / 60);
-        $diffInSeconds %= 60;
-        $seconds = $diffInSeconds;
+        $years = (int)$diffInDays / Constants::YEAR_IN_DAYS;
+        $months = (int)$diffInDays / Constants::MONTH_IN_DAYS;
+        $days = $this->diffInDays($instance) % Constants::MONTH_IN_DAYS;
+        $hours = $this->diffInHours($instance) % Constants::HOURS_PER_DAY;
+        $minutes = $this->diffInMinutes($instance) % Constants::MINUTES_PER_HOUR;
+        $seconds = $diffInSeconds % Constants::SECONDS_PER_MINUTE;
 
         $diff = new \stdClass();
-        $diff->years = $years;
-        $diff->months = $months;
-        $diff->days = $days;
-        $diff->hours = $hours;
-        $diff->minutes = $minutes;
-        $diff->seconds = $seconds;
+        $diff->years = (int)$years;
+        $diff->months = (int)$months;
+        $diff->days = (int)$days;
+        $diff->hours = (int)$hours;
+        $diff->minutes = (int)$minutes;
+        $diff->seconds = (int)$seconds;
 
         return $diff;
     }
 
-    public function diffInSeconds($secondInstance)
+    /**
+     * @param CalendarManager instance
+     * @return int
+     */
+    public function diffInSeconds($instance)
     {
-        $diffInSeconds = abs($this->getTimestamp() - $secondInstance->getTimestamp());
+        $diffInSeconds = abs($this->getTimestamp() - $instance->getTimestamp());
 
         return $diffInSeconds;
     }
 
-    public function diffInMinutes($secondInstance)
+    /**
+     * @param CalendarManager instance
+     * @return int
+     */
+    public function diffInMinutes($instance)
     {
-        $diffInSeconds = abs($this->getTimestamp() - $secondInstance->getTimestamp());
-        $diffInMinutes = $diffInSeconds > 60 ? floor($diffInSeconds / 60) : 0;
+        $diffInSeconds = abs($this->getTimestamp() - $instance->getTimestamp());
+        $diffInMinutes = $diffInSeconds >= Constants::SECONDS_PER_MINUTE ? (int)($diffInSeconds / Constants::SECONDS_PER_MINUTE) : 0;
 
         return $diffInMinutes;
     }
 
-    public function diffInHours($secondInstance)
+    /**
+     * @param CalendarManager instance
+     * @return int
+     */
+    public function diffInHours($instance)
     {
-        $diffInSeconds = abs($this->getTimestamp() - $secondInstance->getTimestamp());
-        $diffInHours = $diffInSeconds > 3600 ? floor($diffInSeconds / 3600) : 0;
+        $diffInSeconds = abs($this->getTimestamp() - $instance->getTimestamp());
+        $diffInHours = $diffInSeconds >= Constants::HOUR_IN_SECONDS ? (int)($diffInSeconds / Constants::HOUR_IN_SECONDS) : 0;
 
         return $diffInHours;
     }
 
-    public function diffInDays($secondInstance)
+    /**
+     * @param CalendarManager instance
+     * @return int
+     */
+    public function diffInDays($instance)
     {
-        $diffInSeconds = abs($this->getTimestamp() - $secondInstance->getTimestamp());
-        $diffInDays = $diffInSeconds > 86400 ? floor($diffInSeconds / 86400) : 0;
+        $diffInSeconds = abs($this->getTimestamp() - $instance->getTimestamp());
+        $diffInDays = $diffInSeconds >= Constants::DAY_IN_SECONDS ? (int)($diffInSeconds / Constants::DAY_IN_SECONDS) : 0;
 
         return $diffInDays;
     }
 
-    public function diffInMonths($secondInstance)
+    /**
+     * @param CalendarManager instance
+     * @return int
+     */
+    public function diffInMonths($instance)
     {
-        $diffInSeconds = abs($this->getTimestamp() - $secondInstance->getTimestamp());
-        $diffInMonths = $diffInSeconds > 2592000 ? floor($diffInSeconds / 2592000) : 0;
+        $diffInSeconds = abs($this->getTimestamp() - $instance->getTimestamp());
+        $diffInMonths = $diffInSeconds >= Constants::MONTH_IN_SECONDS ? (int)($diffInSeconds / Constants::MONTH_IN_SECONDS) : 0;
 
         return $diffInMonths;
     }
 
-    public function diffInYears($secondInstance)
+    /**
+     * @param CalendarManager instance
+     * @return int
+     */
+    public function diffInYears($instance)
     {
-        $diffInSeconds = abs($this->getTimestamp() - $secondInstance->getTimestamp());
-        $diffInYears = $diffInSeconds > 31536000 ? floor($diffInSeconds / 31536000) : 0;
+        $diffInSeconds = abs($this->getTimestamp() - $instance->getTimestamp());
+        $diffInYears = $diffInSeconds >= Constants::YEAR_IN_SECONDS ? (int)($diffInSeconds / Constants::YEAR_IN_SECONDS) : 0;
 
         return $diffInYears;
     }
