8c5744ace7f804d39551ff40153c579cbf074875
Majid Ataee <ataiemajid_63@hotmail.com>
2020-11-09 18:13:17 +0330

    Test new parser.

--- tests/PasoonateTest.php
+++ tests/PasoonateTest.php
@@ -8,7 +8,8 @@ class CalendarTest extends TestCase
     public function testJalaliDateTime()
     {
         $pasoonate = Pasoonate::make();
+        $date = $pasoonate->jalali()->parse('yyyy/MM/dd HH:mm:ss', '1399/10/12 20:12:00')->format('yyyy/MM/dd HH:mm:ss');
 
-        $this->assertEquals($pasoonate->jalali('1398/12/10')->format('yyyy/MM/dd'), '1398/12/10', 'Jalali Date is ok');
+        $this->assertEquals($date, '1399/10/12 20:12:00', 'Jalali Date is ok');
     }
 }
