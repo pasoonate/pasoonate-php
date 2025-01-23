2b86832ce5b56e8b005a6cb8622fffa2cf97a435
Mahdi Bagheri <mb.reckless@gmail.com>
2025-01-23 19:14:53 +0330

    fix(test): some restructure

--- tests/PasoonateTest.php
+++ tests/PasoonateTest.php
@@ -9,33 +9,32 @@ class PasoonateTest extends TestCase
     {
         $pasoonate = Pasoonate::make();
         $date = $pasoonate->jalali()->parse('yyyy/MM/dd HH:mm:ss', '1404/01/01 20:12:00')->format('yyyy/MM/dd HH:mm:ss');
-
         $this->assertEquals($date, '1404/01/01 20:12:00', 'Jalali Date is ok');
     }
-    
+
     public function testGregorianParser()
     {
         $pasoonate = Pasoonate::make();
-        $datetime = $pasoonate->gregorian()->parse('yyyy/MM/dd HH:mm:ss', '2023/02/12 14:00:00')->format('HH:mm');
-
-        $this->assertEquals($datetime, '14:00', 'Gregorian Time is ok');
-
+        $time = $pasoonate->gregorian()->parse('yyyy/MM/dd HH:mm:ss', '2023/02/12 14:00:00')->format('HH:mm');
+        $this->assertEquals($time, '14:00', 'Gregorian Time is ok');
 
         $pasoonate = Pasoonate::make();
-        $datetime = $pasoonate->gregorian()->parse('yyyy/MM/dd HH:mm:ss', '2023/02/12 14:20:00')->format('HH:mm');
+        $time = $pasoonate->gregorian()->parse('yyyy/MM/dd HH:mm:ss', '2023/02/12 14:20:00')->format('HH:mm');
+        $this->assertEquals($time, '14:20', 'Gregorian Time is ok');
 
-        $this->assertEquals($datetime, '14:20', 'Gregorian Time is ok');
+        $pasoonate = Pasoonate::make();
+        $date = $pasoonate->gregorian()->parse('yyyy/MM/dd HH:mm:ss', '2025/01/23 14:20:00')->format('yyyy/MM/dd HH:mm:ss');
+        $this->assertEquals($date, '2025/01/23 14:20:00', 'Gregorian Date is ok');
     }
 
-    public function testGaregorianParser1()
+    public function testCalendarConvert()
     {
         $pasoonate = Pasoonate::make();
-        $pasoonate->gregorian()->parse('yyyy-MM-dd HH:mm:dd', '2021-10-01 13:21:25');
+        $date = $pasoonate->gregorian()->parse('yyyy-MM-dd HH:mm:dd', '2025-01-23 13:21:25')->jalali()->format('dddd, dd MMMM yyyy - H:m');
+        $this->assertEquals($date, 'پنج‌شنبه, 04 بهمن 1403 - 13:21', 'Garegorian to Jalali is ok');
 
-        $date = $pasoonate->jalali()->format('dddd, dd MMMM yyyy - H:m');
-
-        $this->assertEquals($date, 'دوشنبه, 03 آبان 1400 - 13:21', 'Garegorian Date is ok');
-
-        $this->assertEquals($pasoonate->gregorian("2023-02-03 23:59:58")->jalali()->format('yyyy-MM-dd'), '1401-11-14');
+        $pasoonate = Pasoonate::make();
+        $date = $pasoonate->gregorian("2023-02-03 23:59:58")->jalali()->format('yyyy-MM-dd');
+        $this->assertEquals($date, '1401-11-14');
     }
 }
